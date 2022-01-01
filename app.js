c8c1f3aceff1d0483b24b88dca24234650aca811
gxuud <igaoxian@foxmail.com>
2022-01-01 23:00:19 +0800

    feat: 缓存、爬数据之后保留格式、请求超时重新请求

--- app.js
+++ app.js
@@ -4,11 +4,13 @@
 const cheerio = require("cheerio");
 const request = require("request");
 const fs = require("fs");
+const fsPromises = require("fs/promises");
 const path = require("path");
 let count = 0; //叠加
-let url = 'http://www.23us.cc/html/150/150567/'; //小说Url
+let url = 'http://www.81zw.me/book/354356/'; //小说Url
 let list = []; //章节List
 let booksName = ''; //小说名称
+let read = './read.json'; // 配置文件位置
 
 /**
  * 获取小说目录页
@@ -27,30 +29,52 @@ const books = function () {
  * 处理小说名称及其小说目录
  * @param {*} body 
  */
-const booksQuery = function (body) {
-    $ = cheerio.load(body);
-    booksName = $('.btitle').find('h1').text(); //小说名称
-    $('.chapterlist').find('a').each(function (i, e) { //获取章节UrlList
-        list.push($(e).attr('href'))
-    });
-    createFolder(path.join(__dirname, `/book/${booksName}.txt`)); //创建文件夹
-    fs.createWriteStream(path.join(__dirname, `/book/${booksName}.txt`)) //创建txt文件
+async function booksQuery(body) {
+    if (!booksName) {
+        $ = cheerio.load(body);
+        booksName = $('#info').find('h1').text().trim(); //小说名称
+        $('#list').find('a').each(function (i, e) { //获取章节UrlList
+            list.push({ key: $(e).attr('href'), title: $(e).text(), isLoad: false })
+        });
+        await setReadJson(list)
+    }
+    if (await !fs.existsSync(`/book/${booksName}.txt`)) {
+        createFolder(path.join(__dirname, `/book/${booksName}.txt`)); //创建文件夹
+        fs.createWriteStream(path.join(__dirname, `/book/${booksName}.txt`)) //创建txt文件
+    }
+
     console.log(`开始写入《${booksName}》·······`)
     getBody(); //获取章节信息
-
 }
 /**
  * 获取章节页面信息
  * 
  */
 const getBody = function () {
-    let primUrl = url + list[count];
-    // console.log(primUrl)
-    request(primUrl, function (err, res, body) {
+    const rr = require('./read.json');
+    const readItem = rr.find((item) => !item.isLoad);
+    if (!readItem.key) {
+        console.log('下载完成！！！！！！');
+        return;
+    }
+    Object.keys(require.cache).forEach(function(key) {
+    delete require.cache[key];
+    })
+    let primUrl = url + readItem.key;
+    console.log(readItem)
+    request(primUrl, { timeout: 10000 }, async function (err, res, body) {
         if (!err && res.statusCode == 200) {
+            await setReadJson(rr.map((item) => {
+                if (item.key === readItem.key) {
+                    return { key: item.key, isLoad: true };
+                }
+                return item;
+            }))
             toQuery(body);
+            
         } else {
             console.log('err:' + err)
+            getBody()
         }
     })
 };
@@ -58,23 +82,20 @@ const getBody = function () {
  * 处理章节页面信息
  * @param {any} body 
  */
-const toQuery = function (body) {
+async function toQuery(body) {
     $ = cheerio.load(body);
     const title = $('h1').text(); //获取章节标题
-    const content = Trim($('#content').text(), 'g'); //获取当前章节文本内容并去除文本所有空格
-    writeFs(title, content);
+    const content = String($('#content').html()).replace(/<br>/g, '\n').replace('<script type="text/javascript" src="/js/chaptererror.js"></script>', ''); //获取当前章节文本内容并去除文本所有空格
+    await writeFs(title, content.trim());
 }
 /**
  * 写入txt文件
  * @param {*} title 
  * @param {*} content 
  */
-const writeFs = function (title, content) {
+async function writeFs(title, content) {
     // 添加数据
-    fs.appendFile(path.join(__dirname, `/book/${booksName}.txt`), title, function (err) {
-        if (err) throw err;
-    });
-    fs.appendFile(path.join(__dirname, `/book/${booksName}.txt`), content, function (err) {
+    fs.appendFile(path.join(__dirname, `/book/${booksName}.txt`), `\n${title}\n　　${content}`, function (err) {
         if (err) {
             console.log(err)
         } else {
@@ -103,21 +124,14 @@ const createFolder = function (to) { //文件写入
     }
 };
 
-/**
- * 
- * 去除所有空格
- * @param {any} str 
- * @param {any} is_global 
- * @returns 
- */
-const Trim = function (str, is_global) {
-
-    var result;
-    result = str.replace(/(^\s+)|(\s+$)/g, "");
-    if (is_global.toLowerCase() == "g")
-    {
-        result = result.replace(/\s/g, "");
+async function setReadJson(params) {
+    if (await fs.existsSync(read)) {
+        createFolder(read);
     }
-    return result;
+    await fsPromises.writeFile(path.join(__dirname, read), JSON.stringify(params, null, 2), function (err) {
+        if (err) throw err;
+    });
 }
+
+
 books();
